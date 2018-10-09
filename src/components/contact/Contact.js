11c8956258037f2d41031a4a01af696f439aabad
hemant <hemant30053@gmail.com>
2018-10-09 23:18:15 +0530

    async await

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -7,9 +7,16 @@ class Contact extends Component {
     state = {
         showContactInfo: false
     }
-    onDeleteClick = (id, dispatch) => {
-        axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`)
-            .then(res => dispatch({type: 'DELETE_CONTACT', payload: id}) )
+    onDeleteClick = async (id, dispatch) => {
+        try {
+            await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`)
+            dispatch({type: 'DELETE_CONTACT', payload: id})
+        }
+        catch(e) {
+            dispatch({type: 'DELETE_CONTACT', payload: id})
+            console.log(e);
+
+        }
 
     }
 
